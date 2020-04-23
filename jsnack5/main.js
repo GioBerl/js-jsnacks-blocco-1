4ddfc41640996a8ce68ed31e528963f6130650a3
Gio <giovanni.berl1@gmail.com>
2020-04-23 15:29:41 +0200

    corretto snack6 e modificato snack5 per comprendere anche lo 0

--- jsnack5/main.js
+++ jsnack5/main.js
@@ -1,14 +1,14 @@
 // Stampa il cubo dei primi N numeri, dove N è un numero indicato dall'utente
 
 // versione faidate
-for (var i = 1; i < numero + 1; i++) {
+var numero = parseInt(prompt("stampa il cubo dei numeri fino a: "));
+for (var i = 0; i < numero + 1; i++) {
     var cubo = i * i * i;
     console.log(i + " elevato al cubo mi da: " + cubo);
 }
 
 // versione semplificata
-// var numero = parseInt(prompt("stampa il cubo dei numeri fino a: "));
 
-// for (var i = 1; i <= numero; i++) {
+// for (var i = 0; i <= numero; i++) {
 //     console.log(i + " elevato al cubo ---> ", Math.pow(i, 3));
 // }
--- jsnack6/main.js
+++ jsnack6/main.js
@@ -1,12 +1,6 @@
 // Stampa le potenze di 2 fino a 1000
 
-// versione faidate
-for (var i = 1; i < 1001; i++) {
+for (var i = 0; 2 ** i < 1000; i++) {
     var totale = 2 ** i;
     console.log("la potenza di 2 elevato a " + i + " vale: " + totale);
 }
-
-// versione semplificata
-// for (var i = 1; i < 1001; i++) {
-//     console.log("2 elevato a " + i + " --->", Math.pow(2, i));
-// }
