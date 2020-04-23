f939ebee5697634af6a8001f8e7f39286ab1fe2d
Gio <giovanni.berl1@gmail.com>
2020-04-23 14:13:59 +0200

    aggiunte versioni faidate

--- jsnack6/main.js
+++ jsnack6/main.js
@@ -1,6 +1,12 @@
 // Stampa le potenze di 2 fino a 1000
 
-// versione semplificata
+// versione faidate
 for (var i = 1; i < 1001; i++) {
-    console.log("2 elevato a " + i + " --->", Math.pow(2, i));
+    var totale = 2 ** i;
+    console.log("la potenza di 2 elevato a " + i + " vale: " + totale);
 }
+
+// versione semplificata
+// for (var i = 1; i < 1001; i++) {
+//     console.log("2 elevato a " + i + " --->", Math.pow(2, i));
+// }
